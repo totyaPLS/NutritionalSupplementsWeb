a42a1e9b32edad4bf7082fecd3379d5b85e3a60e
Ákos-laptop <totyavidpulse@gmail.com>
2023-04-27 12:03:06 +0200

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { MenuComponent } from './common/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CartComponent,
+    LoginComponent,
+    RegistrationComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
