b4a64617d0f80caf7241a243108717e4248af856
Ákos-PC <totyavidpulse@gmail.com>
2023-05-01 02:24:45 +0200

    * validators implemented on signup

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, ValidationErrors, Validators} from "@angular/forms";
 import {Location} from "@angular/common";
 import {User} from "../../common/models/User";
 
@@ -18,10 +18,9 @@ export class RegistrationComponent implements OnInit {
   rePassword = new FormControl('');
 
   constructor(private location: Location, private fb: FormBuilder) {
-    this.rePassword.addValidators([Validators.required, Validators.minLength(8)]);
+    this.rePassword.addValidators([Validators.required]);
     this.rePassword.valueChanges.subscribe(() => {
       this.checkPasswordsMatch(this.signUpForm.get('password')?.value, this.rePassword);
-      // if (!this.rePassword.valid) console.log("Hmm, mismatch...");
     });
   }
 
@@ -29,8 +28,17 @@ export class RegistrationComponent implements OnInit {
 
   createForm(model: User) {
     let formGroup = this.fb.group(model);
-    formGroup.get('email')?.addValidators(Validators.required);
-    formGroup.get('password')?.addValidators([Validators.required, Validators.minLength(8)]);
+
+    formGroup.get('email')?.addValidators([
+      Validators.required,
+      Validators.email
+    ]);
+    formGroup.get('password')?.addValidators([
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^[a-zA-Z0-9]+$/)
+    ]);
+
     return formGroup;
   }
 
@@ -40,6 +48,54 @@ export class RegistrationComponent implements OnInit {
     } else {
       rePassword.setErrors({ mismatch: true });
     }
+  }
+
+  getFormValidationErrors() {
+    Object.keys(this.signUpForm.controls).forEach(key => {
+      // @ts-ignore
+      const controlErrors: ValidationErrors = this.signUpForm.get(key).errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(keyError => {
+          console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+        });
+      }
+    });
+  }
+
+  getEmailError() {
+    if (this.signUpForm.controls['email'].hasError('required')) {
+      return 'This field is required';
+    }
+
+    if (this.signUpForm.controls['email'].hasError('email')) {
+      return 'Please give a valid e-mail format';
+    }
+
+    return '';
+  }
+
+  getPasswordError() {
+    if (this.signUpForm.controls['password'].hasError('required')) {
+      return 'This field is required.';
+    }
+
+    if ((this.signUpForm.controls['password'].hasError('pattern'))) {
+      return 'No special characters allowed';
+    }
+
+    if (!(this.signUpForm.controls['password'].hasError('minLength'))) {
+      return 'Too short (min. 8 character)';
+    }
+
+    return '';
+  }
+
+  getRePasswordError() {
+    if (this.rePassword.hasError('mismatch')) {
+      return 'This does not match with the password';
+    }
+
+    return '';
   }
 
   onSubmit() {
