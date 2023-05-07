69d67f075b85fc9f8f2960b062d55d5c0fe481d1
Ákos-PC <totyavidpulse@gmail.com>
2023-05-07 23:50:13 +0200

    main * gitignore + environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'nutritional-supplements-5b5aa',
+    appId: '1:1015155294016:web:15e2074c5c5531dd5b84a5',
+    storageBucket: 'nutritional-supplements-5b5aa.appspot.com',
+    apiKey: 'AIzaSyA0eqFuOaF5aEaVfwvEWqbvyi3Xb_VEAoY',
+    authDomain: 'nutritional-supplements-5b5aa.firebaseapp.com',
+    messagingSenderId: '1015155294016',
+    measurementId: 'G-FQBK0HS39Y',
+  },
+  production: false
+};
